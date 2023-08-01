ea52c51dc19707c04a4cb194ed741045d7090d67
choengilkim <clamp222@naver.com>
2023-08-01 18:42:47 +0900

    first commit

---
+++ src/test/java/com/sbs/exam/sb_app/SbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
