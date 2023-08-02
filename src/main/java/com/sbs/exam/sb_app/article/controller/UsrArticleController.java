75eef092edbf4ffd6caf142969c91552f80c305a
choengilkim <clamp222@naver.com>
2023-08-02 20:19:47 +0900

    게시물 crud구현

---
+++ src/main/java/com/sbs/exam/sb_app/article/controller/UsrArticleController.java
@@ -0,0 +1,92 @@
+package com.sbs.exam.sb_app.article.controller;
+
+import com.sbs.exam.sb_app.article.vo.Article;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UsrArticleController {
+
+  // 인스턴스 변수 시작
+  int articleLastId;
+  private List<Article> articles;
+  // 인스턴스 변수 끝
+
+  // 생성자
+  public UsrArticleController() {
+    articles = new ArrayList<>();
+    articleLastId = 0;
+
+    makeTestData();
+  }
+
+  // 서비스 메소드 시작
+  private void makeTestData() {
+    for(int i=1; i<=10; i++) {
+      String title = "제목" + i;
+      String body = "내용" + i;
+
+      writeArticle(title, body);
+    }
+  }
+
+  private Article getArticle(int id) {
+    for(Article article : articles) {
+      if(article.getId() == id) {
+        return article;
+      }
+    }
+    return null;
+  }
+  private Article writeArticle(String title, String body) {
+    int id = articleLastId + 1;
+    Article article = new Article(id, title, body);
+
+    articles.add(article);
+    articleLastId = id;
+
+    return article;
+  }
+  private void deleteArticle(int id) {
+    Article article = getArticle(id);
+    articles.remove(article);
+  }
+  // 서비스 메소드 끝
+  
+  // 액션 메소드 시작  
+  @RequestMapping("/usr/article/doAdd")
+  @ResponseBody
+  public Article doAdd(String title, String body) {
+    Article article = writeArticle(title, body);
+
+    return article;
+  }
+
+  @RequestMapping("/usr/article/getArticles")
+  @ResponseBody
+  public List<Article> getArticles() {
+    return articles;
+  }
+
+  @RequestMapping("/usr/article/doDelete")
+  @ResponseBody
+  public String doDelete(int id) {
+    Article article = getArticle(id);
+
+    if(article == null) {
+      return id + "번 게시물이 존재하지 않습니다.";
+    }
+    deleteArticle(id);
+
+    return id + "번 게시물을 삭제하였습니다.";
+  }
+
+
+  // 액션 메소드 끝
+
+
+}
