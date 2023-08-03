fa64e1a083fa58e18b871cd6ecaf1c27d3f3adca
choengilkim <clamp222@naver.com>
2023-08-03 20:32:48 +0900

    DB스키마생성

---
+++ src/main/java/com/sbs/exam/sb_app/article/service/ArticleService.java
@@ -0,0 +1,39 @@
+package com.sbs.exam.sb_app.article.service;
+
+import com.sbs.exam.sb_app.article.repository.ArticleRepository;
+import com.sbs.exam.sb_app.article.vo.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+  private ArticleRepository articleRepository;
+
+  public ArticleService(ArticleRepository articleRepository) {
+    this.articleRepository = articleRepository;
+    articleRepository.makeTestData();
+  }
+
+  public Article writeArticle(String title, String body) {
+    return articleRepository.writeArticle(title, body);
+  }
+
+  public List<Article> getArticles() {
+    return articleRepository.getArticles();
+  }
+
+  public Article getArticle(int id) {
+    return articleRepository.getArticle(id);
+  }
+
+  public void deleteArticle(int id) {
+    articleRepository.deleteArticle(id);
+  }
+
+  public void modifyArticle(int id, String title, String body) {
+    articleRepository.modifyArticle(id, title, body);
+  }
+}
